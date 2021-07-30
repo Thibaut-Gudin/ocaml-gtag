7787b2b40f5c9ec0349c4637e5db007949c1eb18
Thibaut <tgudin@besport.com>
2021-07-30 17:52:20 +0200

    adding gtag function into .mli file

--- src/gtag.mli
+++ src/gtag.mli
@@ -1,1 +1,12 @@
-(*TODO*)
+val gtag : event:string -> event_name:string -> Ojs.t -> unit
+  [@@js.global "gtag"]
+
+[@@@js.stop]
+
+val available : unit -> bool
+
+[@@@js.start]
+
+[@@@js.implem
+let available () =
+  Js_of_ocaml.Js.Optdef.test Js_of_ocaml.Js.Unsafe.global##.gtag]
